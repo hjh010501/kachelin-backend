ca7f1a209d320817209e252f1408f32058485ca4
kidevelop <hjh010501@naver.com>
2020-06-05 00:52:11 +0900

    (fix): unset auth at restaurant get method

--- backend/views/api/restaurant.py
+++ backend/views/api/restaurant.py
@@ -3,14 +3,15 @@ from rest_framework.viewsets import ReadOnlyModelViewSet, ModelViewSet
 from backend.models.restaurant import Restaurant
 from backend.serializers.restaurant import RestaurantSerializer, RestaurantCreateSerializer
 from rest_framework.decorators import action, api_view, permission_classes, authentication_classes
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 
 class RestaurantViewSet(ModelViewSet):
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticatedOrReadOnly,)
     authentication_classes = ((JSONWebTokenAuthentication,)) 
     queryset = Restaurant.objects.all()
     serializer_class = RestaurantSerializer
+
     def get_serializer_class(self):
         if self.action == 'create':
             return RestaurantCreateSerializer
